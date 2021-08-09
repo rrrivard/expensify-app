149cfe109335a368decef6ec89ae96ed16d6282c
Richard R Rivard <rickyrivard@me.com>
2021-08-09 13:28:24 -0400

    Add readme

--- src/app.js
+++ src/app.js
@@ -12,11 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-
-
-const state = store.getState();
-
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
